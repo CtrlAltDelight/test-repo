e7005614159f68d3bbdf87d53126cff8cf85a7f3
CtrlAltDelight <lbchigges@gmail.com>
2023-08-29 20:08:42 -0400

    Made hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hello, world!\n");
+	return EXIT_SUCCESS;
+}
+
+/* vim: set tabstop=4 shiftwidth=4 fileencoding=utf-8 noexpandtab: */
